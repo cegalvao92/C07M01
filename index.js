d331d559df845f6169ad9a13f3d8c676518fe0e5
cegalvao92 <83725112+cegalvao92@users.noreply.github.com>
2021-08-18 20:01:11 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log('Cadastro de Clientes \n')
+console.log('0 - FIM \n')
+console.log('1 - Inclui \n')
+console.log('2 - Altera \n')
+console.log('3 - Exclui \n')
+console.log('4 - Consulta \n')
+
+let opc = prompt("Digite a opção: ")
+console.log(opc)
+
+
+
